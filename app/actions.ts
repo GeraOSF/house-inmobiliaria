0030b2d01406d5a30a18ad5ee6a8ef66db7a6578
GeraOSF <gerardogomezromero175@gmail.com>
2024-07-03 00:13:21 -0700

    chore: actually use actions

--- app/actions.ts
+++ app/actions.ts
@@ -1,8 +1,9 @@
+"use server";
 import { z } from "zod";
-
 import { db } from "@/lib/prisma";
 import { propertySchema, editPropertySchema } from "@/lib/validations";
-import { User } from "@clerk/nextjs/server";
+import { auth, clerkClient, type User } from "@clerk/nextjs/server";
+import { utapi } from "@/lib/uploadthing-server";
 
 export async function getProperties() {
   return await db.property.findMany({
@@ -11,29 +12,54 @@ export async function getProperties() {
 }
 
 export async function createProperty(data: z.infer<typeof propertySchema>) {
-  await fetch("/api/property", {
-    method: "POST",
-    body: JSON.stringify(data),
-  });
+  if (!hasPermissions(["isAdmin", "canAddProperties"]))
+    throw new Error("Unauthorized");
+  try {
+    await db.property.create({ data });
+    return;
+  } catch (error) {
+    console.error(error);
+    if (data.imageKeys.length) {
+      await utapi.deleteFiles(data.imageKeys);
+    }
+  }
 }
 
 export async function editProperty(data: z.infer<typeof editPropertySchema>) {
-  await fetch("/api/property", {
-    method: "PATCH",
-    body: JSON.stringify(data),
-  });
+  if (!hasPermissions(["isAdmin"])) throw new Error("Unauthorized");
+  try {
+    await db.property.update({ where: { id: data.id }, data });
+    return;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function deleteProperty(id: number) {
-  return await fetch("/api/property", {
-    method: "DELETE",
-    body: JSON.stringify({ id }),
-  });
+  if (!hasPermissions(["isAdmin"])) throw new Error("Unauthorized");
+  try {
+    const property = await db.property.findUnique({
+      select: { imageKeys: true },
+      where: { id },
+    });
+    if (property && property.imageKeys.length) {
+      await utapi.deleteFiles(property.imageKeys);
+    }
+    await db.property.delete({ where: { id } });
+    return;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
-export async function getUsers(): Promise<User[]> {
-  const { users } = await fetch("/api/users").then((res) => res.json());
-  return users;
+export async function getUsers(): Promise<User[] | undefined> {
+  if (!hasPermissions(["isAdmin"])) throw new Error("Unauthorized");
+  try {
+    const users = await clerkClient.users.getUserList();
+    return users;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 export async function updateUserPermissions({
@@ -43,8 +69,22 @@ export async function updateUserPermissions({
   userId: string;
   canAddProperties: boolean;
 }) {
-  await fetch("/api/users", {
-    method: "PATCH",
-    body: JSON.stringify({ userId, canAddProperties }),
-  });
+  if (!hasPermissions(["isAdmin"])) throw new Error("Unauthorized");
+  try {
+    const user = await clerkClient.users.getUser(userId);
+    if (!user) {
+      throw new Error("User not found");
+    }
+    await clerkClient.users.updateUserMetadata(userId, {
+      publicMetadata: { canAddProperties },
+    });
+    return;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+function hasPermissions(permissions: ("isAdmin" | "canAddProperties")[]) {
+  const { sessionClaims } = auth();
+  return permissions.every((permission) => sessionClaims?.[permission]);
 }
